00d23ab90514e23c4645e307fe1f5fbeb0665ccc
James Nugent <james.nugent@bathasu.com>
2014-06-09 18:29:00 +0100

    Add all the files

---
+++ SkypeLogViewerLGG/DateTimeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SkypeLogViewerLGG
+{
+    public static class DateTimeExtensions
+    {
+        public static long ToUnixTimestamp(this DateTime target)
+        {
+            var date = new DateTime(1970, 1, 1, 0, 0, 0, target.Kind);
+            var unixTimestamp = Convert.ToInt64((target - date).TotalSeconds);
+
+            return unixTimestamp;
+        }
+    }
+}
